ea20ba6de670a8da045eb83c20eda69424435446
n3tphant0m <89321496+TheBhacker@users.noreply.github.com>
2024-12-15 19:42:48 +0100

    Update target_to_hack.sh

--- config/bspwm/scripts/target_to_hack.sh
+++ config/bspwm/scripts/target_to_hack.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
  
-ip_address=$(/bin/cat /home/d14bl0/.config/bin/target | awk '{print $1}')
-machine_name=$(/bin/cat /home/d14bl0/.config/bin/target | awk '{print $2}')
+ip_address=$(/bin/cat /home/user/.config/bin/target | awk '{print $1}')
+machine_name=$(/bin/cat /home/user/.config/bin/target | awk '{print $2}')
  
 if [ $ip_address ] && [ $machine_name ]; then
     echo "%{F#e51d0b} %{F#ffffff}$ip_address%{u-} - $machine_name"
