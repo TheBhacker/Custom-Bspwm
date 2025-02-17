d456433ed6b915d9b3ba682806d0685b042d3dca
N3tShad0w <89321496+TheBhacker@users.noreply.github.com>
2025-02-17 18:16:50 +0100

    Add files via upload

--- .p10k.zsh
+++ .p10k.zsh
@@ -188,10 +188,10 @@
 
   #################################[ os_icon: os identifier ]##################################
   # OS identifier color.
-  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=7
-  typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=56
+  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=0
+  typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=225
   # Custom icon.
-  typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='󰆨'
+  typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='󰮯'
 
   ################################[ prompt_char: prompt symbol ]################################
   # Transparent background.
