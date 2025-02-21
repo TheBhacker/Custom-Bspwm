76edd57dd17ec6b8d838880753bd7341f8249ac1
N3tShad0w <89321496+TheBhacker@users.noreply.github.com>
2025-02-21 19:38:38 +0100

    Add files via upload

---
+++ config/bspwm/cheatsheet/show-cheatsheet.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Path to the cheatsheet file
+CHEATSHEET_FILE="/home/n3tshad0w/.config/bspwm/cheatsheet/bspwm-cheatsheet.txt"
+
+# Check if the cheatsheet file exists
+if [[ ! -f "$CHEATSHEET_FILE" ]]; then
+  echo "Cheatsheet file not found at $CHEATSHEET_FILE"
+  exit 1
+fi
+
+# Read the cheatsheet file and display it with rofi
+cat "$CHEATSHEET_FILE" | rofi -dmenu -p "Bspwm Cheatsheet" -theme /home/n3tshad0w/.config/bspwm/cheatsheet/cheatsheet.rasi
